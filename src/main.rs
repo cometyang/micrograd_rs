852f0470d111b7048b70979e7d5e0ed9d4f9ec06
cometyang <cometyang@gmail.com>
2022-09-27 21:04:34 -0500

    add graphs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ trait ValuePrint: fmt::Display {
     }
 }
 
-type ValueRef = Option<Rc<Value>>;
+type ValueRef = Option<Box<Value>>;
 
 
 #[derive(Clone, Debug)]
@@ -36,8 +36,8 @@ impl ops::Add for Value {
 
     fn add(self, other: Self) -> Self {
 
-        let left_op = Rc::new(self.clone());
-        let right_op = Rc::new(other.clone());
+        let left_op = Box::new(self.clone());
+        let right_op = Box::new(other.clone());
         Self{ data: self.data+other.data, _prev: (Some(left_op), Some(right_op)), label: "+".to_string() }
     }
 }
@@ -46,8 +46,8 @@ impl ops::Mul for Value {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
-        let left_op = Rc::new(self.clone());
-        let right_op = Rc::new(other.clone());
+        let left_op = Box::new(self.clone());
+        let right_op = Box::new(other.clone());
         Self{ data: self.data*other.data, _prev: (Some(left_op), Some(right_op)), label: '*'.to_string()}
     }
 
@@ -55,16 +55,54 @@ impl ops::Mul for Value {
 
 impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.data)
+        write!(f, "{{ {}| data: {:.4} }}", self.label, self.data)
     }
 }
 
-impl ValuePrint for Value {
+trait Trace {
+    fn trace(&self) -> Graph<&Value, &str>;
+}
+
+use petgraph::graph::Graph;
+use petgraph::graph::NodeIndex;
+
+
+impl Trace for Value {
+    fn trace(&self) -> Graph<&Value, &str> {
+        let mut graph = Graph::<&Value, &str>::new();
+        let origin = graph.add_node(&self);
+        fn build<'a>(graph: &mut Graph<&'a Value, &str>, node: &'a Value, index: NodeIndex) {
+
+            match &node._prev.0 {
+                Some(v) => {
+                    let destination_1 = graph.add_node(&v);
+                    let cost_1 = graph.add_edge(index, destination_1,"");
+                    build(graph, v, destination_1);
+                },
+                None => ()
+            }
+            
+            match &node._prev.1 {
+                Some(v) => {
+                    let destination_1 = graph.add_node(&v);
+                    let cost_1 = graph.add_edge(index, destination_1,"");
+                    build(graph,v, destination_1);
+                },
+                None => ()
+            }
+
+        }
+
+
+        build(&mut graph, &self, origin);
+        graph
+    }
 
 }
 
+use petgraph::dot::{Dot, Config};
 fn main() {
-    println!("Hello, world!");
+
     let a = Value::new(2.0, "a"); 
     let b = Value::new(-3.0, "b");
     let c = Value::new(10.0,"c");
@@ -75,10 +113,12 @@ fn main() {
     let f = Value::new(-2.0, "f");
     let mut L =d*f;
     L.label = 'L'.to_string();
-    //let c = Value{data:10.0, _prev: (None, None)};
-    //let e = a*b + c;
+
     //println!("{:?},{:?},{:?},{:?},{:?}", a,b,c,d,e);
-    println!("{}",L);
+    //println!("{}",L);
+    let graph = L.trace();
+    //println!("{}", Dot::new(&graph));
+    //Dot::with_attr_getters(&graph, config, get_edge_attributes, get_node_attributes);
 }
 
 #[cfg(test)]
@@ -87,8 +127,8 @@ mod test {
     use super::*;
     #[test]
     fn add(){
-        let a = Value{data:2.0, _prev: (None, None), label:'a'.to_string()};
-        let b = Value{data:-3.0, _prev: (None, None), label: 'b'.to_string()};
+        let a = Value::new(2.0,"a"); // Value{data:2.0, _prev: (None, None), label:'a'.to_string()};
+        let b = Value::new(-3.0, "b"); //Value{data:-3.0, _prev: (None, None), label: 'b'.to_string()};
         let c =a+b;
        // let d = a*b;
         assert_eq!(c.data, -1.0);
@@ -97,9 +137,9 @@ mod test {
 
     #[test]
     fn mul(){
-        let a = Value{data:2.0, _prev: (None, None), label: 'a'.to_string()};
-        let b = Value{data:-3.0, _prev: (None, None), label: 'b'.to_string()};
-        let c = Value{data:10.0, _prev: (None, None), label: 'c'.to_string()};
+        let a = Value::new(2.0, "a"); //Value{data:2.0, _prev: (None, None), label: 'a'.to_string()};
+        let b = Value::new(-3.0, "b"); //Value{data:-3.0, _prev: (None, None), label: 'b'.to_string()};
+        let c = Value::new(10.0, "c");//Value{data:10.0, _prev: (None, None), label: 'c'.to_string()};
         let d =a*b+c;
         assert_eq!(d.data, 4.0);
 
